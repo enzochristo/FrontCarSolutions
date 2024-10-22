1c7be0a993d8839db9f606042dbf48291a6e3ecb
Henrique Albuquerque <henriqueaa1@al.insper.edu.br>
2024-10-22 16:23:51 -0300

    Criando o projeto

---
+++ carSolutions-front/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello world</Button>
+    </div>
+  )
+}
