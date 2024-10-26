baa41ac370fda094efdc67eff0d5608b2f4818e5
enzochristo <enzosc1@al.insper.edu.br>
2024-10-26 09:45:50 -0300

    footer pronto, mas sem caminhos

--- carSolutions-front/src/components/footer/index.jsx
+++ carSolutions-front/src/components/footer/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
+import whatsappLogo from '../../assets/whatsapplogo.png';
+import facebookLogo from '../../assets/facebooklogo.png';
+import instagramLogo from '../../assets/instagramlogo.png';
+import loclogo from '../../assets/loclogo.png';
 
 export function Footer() {
   return (
@@ -22,25 +26,41 @@ export function Footer() {
 
         <section>
           <h3>Companhia</h3>
+          <div className='divlocation'>
           <Link to="">Sobre Nós</Link>
-          <a href=""></a>
-          <a href=""></a>
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <div className='location'>
+            <img src ={loclogo} alt="" className='loclogo ' />
+            <a href="https://www.google.com/maps/search/?api=1&query=Avenida+Helio+Pelegrino,+1250,+Vila+Olimpia,+SP" target="_blank" rel="noopener noreferrer">
+                Avenida Helio Pelegrino 1250, Vila Olimpia, SP
+              </a>
+          </div>
+          <div className='location'>
+          <img src ={loclogo} alt="" className='loclogo ' />
+          <a href="https://www.google.com/maps/search/?api=1&query=Rod.+H%C3%A9lio+Smidt+S%2FN,+Guarulhos,+SP" target="_blank" rel="noopener noreferrer">
+                Rod. Hélio Smidt S/N, Guarulhos, SP
+              </a>
+          </div>
+          </div>
         </section>
 
       </div>
 
-      <hr />
-      <div>
-        <p className='carsol'>
+      
+      <div className='embaixo-linha'>
+        <h4 className='carsol'>
           Car solutions
-        </p>
-        <nav>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-        </nav>
+        </h4>
+        <div className="footer-social">
+          <a href="https://wa.me/seuNumeroDeWhatsApp" target="_blank" rel="noopener noreferrer" className='circle'>
+            <img src={whatsappLogo} alt="Whatsapp Logo" className="social-logo" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className='circle'>
+            <img src={facebookLogo} alt="Facebook Logo" className="social-logo" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className='circle'>
+            <img src={instagramLogo} alt="Instagram Logo" className="social-logo" />
+          </a>
+        </div>
       </div>
       
     </footer>
