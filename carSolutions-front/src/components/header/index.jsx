14510518143b278f6d993de167036e728d69fcf6
Henrique Albuquerque <henriqueaa1@al.insper.edu.br>
2024-10-28 00:27:20 -0300

    Arrumando logo

--- carSolutions-front/src/components/header/index.jsx
+++ carSolutions-front/src/components/header/index.jsx
@@ -8,44 +8,39 @@ import {
     DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { User } from "lucide-react";
+import carsolutionslogo from "../../assets/carsolutionslogo.png";
 
 export function Header() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     return (
         <header className="header">
-            <div className="header-container">
-                {/* Logo */}
-                <div className="logo">
-                    <img src="../.scr/.assets/.carsolutionslogo" alt="Logo Car Solutions" />
-                </div>
+            {/* Logo */}
+            <img src={carsolutionslogo} alt="Logo Car Solutions" className='fotologo'/>
 
-                {/* Links de navegação */}
-                <nav className="nav-header">
-                    <Link to="/produtos" className="nav-link">Produtos</Link>
-                    <Link to="/sobrenosgeral" className="nav-link">Sobre Nós</Link>
-                    <Link to="/footer" className="nav-link">Fale Conosco</Link>
-                </nav>
+            {/* Links de navegação */}
+            <Link to="/produtos" className="nav-link">Produtos</Link>
+            <Link to="/sobrenosgeral" className="nav-link">Sobre Nós</Link>
+            <Link to="/footer" className="nav-link">Fale Conosco</Link>
 
-                {/* Dropdown Menu - Ícone do usuário */}
-                <DropdownMenu onOpenChange={(open) => setIsDropdownOpen(open)}>
-                    <DropdownMenuTrigger asChild>
-                        <button
-                            className={`user-icon ${isDropdownOpen ? 'user-icon-active' : ''}`}
-                        >
-                            <User size={32} />
-                        </button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end" className="dropdown-menu">
-                        <DropdownMenuItem asChild>
-                            <Link to="/login" className="dropdown-item">Log In</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem asChild>
-                            <Link to="/cadastro" className="dropdown-item">Cadastre-se</Link>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
+            {/* Dropdown Menu - Ícone do usuário */}
+            <DropdownMenu onOpenChange={(open) => setIsDropdownOpen(open)}>
+                <DropdownMenuTrigger asChild>
+                    <button
+                        className={`user-icon ${isDropdownOpen ? 'user-icon-active' : ''}`}
+                    >
+                        <User size={32} />
+                    </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="dropdown-menu">
+                    <DropdownMenuItem asChild>
+                        <Link to="/login" className="dropdown-item">Log In</Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                        <Link to="/cadastro" className="dropdown-item">Cadastre-se</Link>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </header>
     );
 }
