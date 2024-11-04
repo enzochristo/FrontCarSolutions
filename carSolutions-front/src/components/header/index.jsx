73e0183b82f8c49eb23f4fa59fd1e713163fc457
Henrique Albuquerque <henriqueaa1@al.insper.edu.br>
2024-11-04 20:50:29 -0300

    Header finalizado

--- carSolutions-front/src/components/header/index.jsx
+++ carSolutions-front/src/components/header/index.jsx
@@ -7,13 +7,14 @@ import { logoutUser, getUserData } from '../../services/api';
 
 function Header({ scrollToFooter }) {
   const [user, setUser] = useState(null);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [isFleetDropdownOpen, setIsFleetDropdownOpen] = useState(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Novo estado para o menu lateral
-  const navigate = useNavigate();
-  const dropdownRef = useRef(null);
+  const [menuDropdownOpen, setMenuDropdownOpen] = useState(false);
+  const [userDropdownOpen, setUserDropdownOpen] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const menuDropdownRef = useRef(null);
+  const userDropdownRef = useRef(null);
 
+  // Função para buscar dados do usuário
   const fetchUserData = async () => {
     try {
       const token = localStorage.getItem('access_token');
@@ -27,6 +28,7 @@ function Header({ scrollToFooter }) {
     }
   };
 
+  // Função para logout
   const handleLogout = async () => {
     try {
       const refreshToken = localStorage.getItem('refresh_token');
@@ -40,8 +42,9 @@ function Header({ scrollToFooter }) {
     }
   };
 
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-  const toggleMobileMenu = () => setIsSidebarOpen(!isSidebarOpen); // Abre e fecha a aba lateral
+  const toggleMenuDropdown = () => setMenuDropdownOpen(!menuDropdownOpen);
+  const toggleUserDropdown = () => setUserDropdownOpen(!userDropdownOpen);
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
   useEffect(() => {
     const checkAuth = () => {
@@ -52,21 +55,23 @@ function Header({ scrollToFooter }) {
         setUser(null);
       }
     };
+
     const intervalId = setInterval(checkAuth, 1000);
     return () => clearInterval(intervalId);
   }, [user]);
 
+  // Fechar os dropdowns ao clicar fora deles
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdownOpen(false);
-        setIsFleetDropdownOpen(false);
+      if (menuDropdownRef.current && !menuDropdownRef.current.contains(event.target)) {
+        setMenuDropdownOpen(false);
+      }
+      if (userDropdownRef.current && !userDropdownRef.current.contains(event.target)) {
+        setUserDropdownOpen(false);
       }
     };
     document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
   return (
@@ -79,40 +84,61 @@ function Header({ scrollToFooter }) {
       </div>
 
       <nav className="nav-header">
-        {user?.isfuncionario ? (
-          <Link to="/funcionario/ProdutosCadastrados" className="nav-link">Frota</Link>
-        ) : (
-          <Link to="/cliente/Produtos" className="nav-link">Produtos</Link>
-        )}
+        <div className="nav-link" onClick={toggleMenuDropdown}>
+          Menu
+          {menuDropdownOpen && (
+            <div className="dropdown-menu" ref={menuDropdownRef}>
+              {user ? (
+                user.isfuncionario ? (
+                  <>
+                    <Link to="/funcionario/ProdutosCadastrados" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Estoque</Link>
+                    <Link to="/funcionario/TodasReservas" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Histórico de Reservas</Link>
+                    <Link to="/funcionario/PrincipalFuncionario" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Lembretes</Link>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Página Inicial</Link>
+                    <Link to="/cliente/Produtos" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Produtos</Link>
+                    <Link to="/cliente/MinhasReservas" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Minhas Reservas</Link>
+                  </>
+                )
+              ) : (
+                <>
+                  <Link to="/LoginPageCliente" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Página Inicial</Link>
+                  <Link to="/LoginPageCliente" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Produtos</Link>
+                  <Link to="/LoginPageCliente" className="dropdown-item" onClick={() => setMenuDropdownOpen(false)}>Estoque</Link>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+
         <Link to="/sobrenosgeral" className="nav-link">Sobre Nós</Link>
         <button className="nav-link falaconosco" onClick={scrollToFooter}>Fale Conosco</button>
       </nav>
 
-      <div className="user-menu" ref={dropdownRef}>
+      <div className="user-menu" ref={userDropdownRef}>
         {user ? (
           <>
-            <button className="user-icon" onClick={toggleDropdown}>
+            <button className="user-icon" onClick={toggleUserDropdown}>
               Olá, {user.full_name.split(" ")[0]}
             </button>
-            {dropdownOpen && (
-              <div className="dropdown-menu">
-                <Link to="/cliente/MeusDadosCliente" className="dropdown-item" onClick={() => setDropdownOpen(false)}>Meus Dados</Link>
-                {user?.isfuncionario ? null : (
-                  <Link to="/cliente/MinhasReservas" className="dropdown-item" onClick={() => setDropdownOpen(false)}>Minhas Reservas</Link>
-                )}
+            {userDropdownOpen && (
+              <div className="dropdown-menu drop-cliente">
+                <Link to="/cliente/MeusDadosCliente" className="dropdown-item" onClick={() => setUserDropdownOpen(false)}>Meus Dados</Link>
                 <button className="dropdown-item" onClick={handleLogout}>Logout</button>
               </div>
             )}
           </>
         ) : (
           <>
-            <button className="user-icon" onClick={toggleDropdown}>
+            <button className="user-icon" onClick={toggleUserDropdown}>
               <img src={userIconImage} alt="Ícone de Usuário" className="user-icon-img" />
             </button>
-            {dropdownOpen && (
-              <div className="dropdown-menu">
-                <Link to="/LoginPageCliente" className="dropdown-item" onClick={() => setDropdownOpen(false)}>Login</Link>
-                <Link to="/CadastroCliente" className="dropdown-item" onClick={() => setDropdownOpen(false)}>Cadastro</Link>
+            {userDropdownOpen && (
+              <div className="dropdown-menu drop-cliente">
+                <Link to="/LoginPageCliente" className="dropdown-item" onClick={() => setUserDropdownOpen(false)}>Login</Link>
+                <Link to="/CadastroCliente" className="dropdown-item" onClick={() => setUserDropdownOpen(false)}>Cadastro</Link>
               </div>
             )}
           </>
@@ -125,33 +151,36 @@ function Header({ scrollToFooter }) {
         <span className="hamburger-line"></span>
       </button>
 
-      {/* Sidebar para menu móvel */}
-      {isSidebarOpen && (
-        <div className="sidebar">
-          <div className="sidebar-header">
-            <p>Olá, {user ? user.full_name.split(" ")[0] : "Visitante"}</p>
-            <button className="close-sidebar" onClick={toggleMobileMenu}>&times;</button>
-          </div>
-          <div className="sidebar-links">
-            {user?.isfuncionario ? (
-              <>
-                <Link to="/funcionario/ProdutosCadastrados" onClick={toggleMobileMenu}>Produtos Cadastrados</Link>
-                <Link to="/funcionario/TodasReservas" onClick={toggleMobileMenu}>Todas as Reservas</Link>
-              </>
-            ) : (
-              <>
-                <Link to="/cliente/MeusDadosCliente" onClick={toggleMobileMenu}>Meus Dados</Link>
-                <Link to="/cliente/MinhasReservas" onClick={toggleMobileMenu}>Minhas Reservas</Link>
-              </>
-            )}
-            {user && <button onClick={handleLogout}>Logout</button>}
-            {!user && (
-              <>
-                <Link to="/LoginPageCliente" onClick={toggleMobileMenu}>Login</Link>
-                <Link to="/CadastroCliente" onClick={toggleMobileMenu}>Cadastro</Link>
-              </>
-            )}
-          </div>
+      {isMobileMenuOpen && (
+        <div className="mobile-menu">
+          <button className="close-btn" onClick={toggleMobileMenu}>X</button>
+          <p className="mobile-user-greeting">Olá, {user ? user.full_name.split(" ")[0] : 'Visitante'}</p>
+          <Link to="/" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Página Inicial</Link>
+          <Link to="/sobrenosgeral" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Sobre Nós</Link>
+          <button className="mobile-link" onClick={() => { scrollToFooter(); setIsMobileMenuOpen(false); }}>Fale Conosco</button>
+          {user ? (
+            <>
+              {user.isfuncionario ? (
+                <>
+                  <Link to="/funcionario/ProdutosCadastrados" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Estoque</Link>
+                  <Link to="/funcionario/TodasReservas" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Histórico de Reservas</Link>
+                  <Link to="/funcionario/PrincipalFuncionario" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Lembretes</Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/cliente/Produtos" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Produtos</Link>
+                  <Link to="/cliente/MinhasReservas" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Minhas Reservas</Link>
+                </>
+              )}
+              <Link to="/cliente/MeusDadosCliente" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Meus Dados</Link>
+              <button className="mobile-link" onClick={() => { handleLogout(); setIsMobileMenuOpen(false); }}>Logout</button>
+            </>
+          ) : (
+            <>
+              <Link to="/LoginPageCliente" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Login</Link>
+              <Link to="/CadastroCliente" className="mobile-link" onClick={() => setIsMobileMenuOpen(false)}>Cadastro</Link>
+            </>
+          )}
         </div>
       )}
     </header>
